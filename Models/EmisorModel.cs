2ad0c8d32f62cb47e33fb9e4c2e75160926186f3
Faby121753 <m.f.vasconez@hotmail.com>
2023-06-28 00:14:51 -0500

    Agregar archivos de proyecto.

---
+++ Models/EmisorModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Avance1.Models
+{
+    public class EmisorModel
+    {
+        public string NombreEmisor { get; set; }
+    }
+}
+
---
+++ Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace Avance1.Models
+{
+    public class Usuario
+    {
+        public string Observacion { get; set; }
+        public string NombreEmisor { get; set; }
+    }
+}
+
